d13760f16c4a010802a3719e65625eae436e26b0
sjy366 <sjy20131565@gmail.com>
2019-08-07 20:52:15 +0900

    User model에 token column 추가

--- signin/models.py
+++ signin/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=320)
-    
+    token = models.TextField()
--- signin/views.py
+++ signin/views.py
@@ -13,7 +13,7 @@ import jwt
 CLIENT_ID = '935445294329-5mpgtul9b7ibvc56co139f2ullou1p6f.apps.googleusercontent.com'
 
 def oauth_google(request):
-    try:
+    #try:
         token = request.POST['idtoken']
         user_info = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
@@ -25,22 +25,20 @@ def oauth_google(request):
         user_list = User.objects.filter(email=user_email)
 
         if len(user_list) == 0:
-            user = User.objects.create(name=user_name, email=user_email)
+            user = User.objects.create(name=user_name, email=user_email, token='_')
         else:
             user = user_list[0]
         
-        """
         payload = dict()
         payload['user_id'] = user.id
         payload['email'] = user.email
-        paylaod['exp'] = datetime.datetime.utcnow() + datetime.timedelta(seconds=60*60*24)
-        """
+        payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(seconds=60*60*24)
         
         json_res = dict()
         json_res['redirect_url'] = '/mylist.html'
         json_res['user_id'] = user.id
 
         return JsonResponse(json_res)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        return HttpResponse(status=400)
+    #except:
+        #print("Unexpected error:", sys.exc_info()[0])
+        #return HttpResponse(status=400)
