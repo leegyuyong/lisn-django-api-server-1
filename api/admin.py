5900d86e9861e3d87bcf612cec72514d1149752b
깔롱지기 <pneu1908@naver.com>
2019-09-02 13:49:08 +0900

    directory관련 api 구현

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Note, Audio, Sentence
+from .models import User, Note, Audio, Sentence, Directory
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Note)
 admin.site.register(Audio)
 admin.site.register(Sentence)
+admin.site.register(Directory)
--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ class User(models.Model):
 
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    directory = models.ForeignKey(Directory, on_delete=models.PROTECT, null=True) # Note 테이블에 Directory 연결, Directory 삭제되도 Note 삭제 안됨
     title = models.CharField(max_length=200)
     started_at = models.DateTimeField()
     ended_at = models.DateTimeField()
@@ -25,3 +26,7 @@ class Sentence(models.Model):
     started_at = models.IntegerField()
     ended_at = models.IntegerField()
     content = models.TextField()
+
+class Directory(models.Model):                                  # Directory DB 추가
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
