c1cf044fe85a7490d17f817497535379f2085b78
깔롱지기 <pneu1908@naver.com>
2019-09-02 14:11:46 +0900

    directory api 간단 오류 수정

--- api/models.py
+++ api/models.py
@@ -4,6 +4,10 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=320)
 
+class Directory(models.Model):                                  # Directory DB 추가
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     directory = models.ForeignKey(Directory, on_delete=models.PROTECT, null=True) # Note 테이블에 Directory 연결, Directory 삭제되도 Note 삭제 안됨
@@ -26,7 +30,3 @@ class Sentence(models.Model):
     started_at = models.IntegerField()
     ended_at = models.IntegerField()
     content = models.TextField()
-
-class Directory(models.Model):                                  # Directory DB 추가
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
