b1f1b5a22c72bab476c12bf0cd0debea7f48a9d1
sjy366 <sjy20131565@gmail.com>
2019-09-28 03:41:40 +0900

    기본 API 인증 decorator 완성

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,7 @@
 
 def coerce_to_post(request):
+    if hasattr(request, 'is_coerced'):
+        return
     if request.method == 'PUT' or request.method == 'DELETE':
         method = request.method
         if hasattr(request, '_post'):
@@ -17,3 +19,4 @@ def coerce_to_post(request):
             request.PUT = request.POST
         elif request.method == 'DELETE':
             request.DELETE = request.POST
+        request.is_coerced = True
