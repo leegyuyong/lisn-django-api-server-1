452fcb12fe46c4f1221c67edb723b0a3e28519a9
Jungyeon Sohn <sjy20131565@gmail.com>
2019-11-04 12:37:06 +0000

    현재 편집중인 유저 확인 API 추가

--- api/views/views_edit.py
+++ api/views/views_edit.py
@@ -5,9 +5,10 @@ from django.http import HttpResponse, JsonResponse, QueryDict
 from django.conf import settings
 
 from api.utils import coerce_to_post
-from api.models import Note
+from api.models import User, Note
 from api.auth import auth_user_id, auth_directory_id, auth_note_id, auth_audio_id, auth_note_id_shared, auth_note_id_edit
 
+@auth_user_id
 @auth_note_id_shared
 @auth_note_id_edit
 def edit_mode(request):
@@ -21,6 +22,7 @@ def edit_mode(request):
 
     return HttpResponse(status=200)
 
+@auth_user_id
 @auth_note_id_shared
 def read_mode(request):
     coerce_to_post(request)
@@ -36,10 +38,30 @@ def read_mode(request):
 
     return HttpResponse(status=200)
 
+@auth_note_id_shared
+def who_is_editing(request):
+    note_id = int(request.GET.get('note_id'))
+    note = Note.objects.get(id=note_id)
+
+    json_res = dict()
+    json_res['edit_user_id'] = 'None'
+    json_res['edit_user_name'] = 'None'
+    json_res['edit_user_email'] = 'None'
+
+    if note.edit_user is not None:
+        user = User.objects.get(id=note.edit_user)
+        json_res['edit_user_id'] = note.edit_user
+        json_res['edit_user_name'] = user.name
+        json_res['edit_user_email'] = user.email
+    
+    return JsonResponse(json_res)
+
 @log
 def api_note_edited(request):
     try:
-        if request.method == 'PUT':
+        if request.method == 'GET':
+            return who_is_editing(request)
+        elif request.method == 'PUT':
             return edit_mode(request)
         elif request.method == 'DELETE':
             return read_mode(request)
